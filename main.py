4750aeaa20992fd2a47f406d55d8b2bc972e25de
Po Chun, Lu <oceanus11034@gmail.com>
2018-10-20 22:20:09 +0800

    add ouput of main function

--- main.py
+++ main.py
@@ -10,14 +10,18 @@ def str_to_list(input_data):
     ]
     return data_list
 
-def count_time(func, data_list, minsup):
+def get_results(func, data_list, minsup):
     start_time = time.time()
     fp_results = func(data_list, minsup)
     end_time = time.time()
     duration = end_time - start_time
 
-    return duration
+    return (duration, fp_results)
 
+algorithm_list = {
+    'fp': FPGrowth,
+    'ap': Apriori
+}
 
 if __name__ == '__main__':
     import argparse
@@ -31,16 +35,22 @@ if __name__ == '__main__':
     parser.add_argument('--minsup',
                        default=2,
                        help='input minsup')
+    parser.add_argument('--algorithm',
+                       default='fp',
+                       help='input minsup')
     args = parser.parse_args()
 
     with open(args.input, 'r') as input_file, open(args.output, 'w') as output:
+        output.write('pattern, counts')
+
         data_list = str_to_list(input_file)
 
-        time_list = []
-        algorithm_list = [FPGrowth, Apriori]
+        fp_algorithm = algorithm_list[args.algorithm]
 
-        for fp_algorithm in algorithm_list:
-            fp_duration = count_time(fp_algorithm, data_list, args.minsup)
-            time_list.append(fp_duration)
+        (fp_duration, fp_results) = get_results(fp_algorithm, data_list, args.minsup)
 
-        print(time_list)
+        for key in fp_results.fp_dict:
+            output.write(str(key) + ':' + str(fp_results.fp_dict[key]))
+            output.write('\n')
+
+        print(fp_duration)
