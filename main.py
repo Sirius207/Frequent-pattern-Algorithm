9cd85c316a38da328d4dbb051f061afd35786199
Po Chun, Lu <oceanus11034@gmail.com>
2018-10-20 18:21:28 +0800

    update count time function

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ import time
 from algorithm.fpGrowth import FPGrowth
 from algorithm.apriori import Apriori
 
-# Constants
-MINSUP = 2
-
 def str_to_list(input_data):
     data_list = [
         data.replace(',\n', '').split(',')
@@ -13,10 +10,13 @@ def str_to_list(input_data):
     ]
     return data_list
 
-def count_time(func):
+def count_time(func, data_list, minsup):
     start_time = time.time()
-    fp_results = func(data_list, MINSUP)
+    fp_results = func(data_list, minsup)
     end_time = time.time()
+    duration = end_time - start_time
+
+    return duration
 
 
 if __name__ == '__main__':
@@ -35,20 +35,12 @@ if __name__ == '__main__':
 
     with open(args.input, 'r') as input_file, open(args.output, 'w') as output:
         data_list = str_to_list(input_file)
-        print(data_list)
 
         time_list = []
+        algorithm_list = [FPGrowth, Apriori]
 
-        start_time = time.time()
-        fp_results = FPGrowth(data_list, MINSUP)
-        end_time = time.time()
-        duration = end_time - start_time
-        time_list.append(duration)
-
-        start_time = time.time()
-        ap_results = Apriori(data_list, MINSUP)
-        end_time = time.time()
-        duration = end_time - start_time
-        time_list.append(duration)
+        for fp_algorithm in algorithm_list:
+            fp_duration = count_time(fp_algorithm, data_list, args.minsup)
+            time_list.append(fp_duration)
 
         print(time_list)
